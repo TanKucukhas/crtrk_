d8763c1a27a7a4c593d0d3d861a315ca464dead4
Tan Kucukhas <tankucukhas@hotmail.com>
2018-03-17 15:37:42 -0400

    initial commit

---
+++ src/hello.tsx
@@ -0,0 +1,12 @@
+// import * as React from 'react';
+//
+// function Hello(name, level) {
+//   return (
+//     <div>
+//       <p>name, level</p>
+//     </div>
+//   )
+// }
+//
+// export default Hello;
+//
---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
